f6f27166283ff5c4b3f04252f3c39c0d528b3b78
Gustavo Santos <gustappsantos@gmail.com>
2023-01-04 19:30:23 -0300

    Adds PlayerLoopSystemSubscription<T>

---
+++ Assets/UnityMainThreadDispatcher/PlayerLoopSystemSubscription.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine.LowLevel;
+
+public class PlayerLoopSystemSubscription<T> : IDisposable
+{
+    private readonly Action _callback;
+
+    public PlayerLoopSystemSubscription(Action callback)
+    {
+        _callback = callback;
+        Subscribe();
+    }
+
+    private void Invoke()
+    {
+        _callback.Invoke();
+    }
+
+    private void Subscribe()
+    {
+        var loop = PlayerLoop.GetCurrentPlayerLoop();
+        ref var system = ref loop.Find<T>();
+        system.updateDelegate += Invoke;
+        PlayerLoop.SetPlayerLoop(loop);
+    }
+
+    private void Unsubscribe()
+    {
+        var loop = PlayerLoop.GetCurrentPlayerLoop();
+        ref var system = ref loop.Find<T>();
+        system.updateDelegate -= Invoke;
+        PlayerLoop.SetPlayerLoop(loop);
+    }
+
+    public void Dispose()
+    {
+        Unsubscribe();
+    }
+}
